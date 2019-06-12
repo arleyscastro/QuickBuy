b943e2dcba0c7de6dde88fc4ce71b15c13a81dfa
arleyscastro <arleys.castro@gmail.com>
2019-06-12 13:49:47 -0300

    Ciação da entidades

---
+++ QuickBuy.Dominio/Enumeradores/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace QuickBuy.Dominio.Enumeradores
+{
+    public enum TipoFormaPagamentoEnum
+    {
+        NaoDefinido = 0,
+        Boleto = 1,
+        Cartao = 2,
+        Deposito = 3
+    }
+}
---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,40 @@
+﻿using QuickBuy.Dominio.Enumeradores;
+using System;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nomme { get; set; }
+        public DateTime DataPedido { get; set; }
+        public bool EhBoleto
+        {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.Boleto;
+            }
+        }
+        public bool Ehcartao
+        {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.Cartao;
+            }
+        }
+        public bool EhDeposito
+        {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.Deposito;
+            }
+        }
+        public bool EhNaoDefinido
+        {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.NaoDefinido;
+            }
+        }
+    }
+}
