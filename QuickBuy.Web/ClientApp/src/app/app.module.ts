8a6224a0e3ee603aad182c6597ff7f69ef763c17
arleyscastro <arleys.castro@gmail.com>
2019-07-08 14:05:53 -0300

    novas implements

--- QuickBuy.Web/ClientApp/src/app/app.module.ts
+++ QuickBuy.Web/ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ProdutoComponent } from './produto/produto.component';
+import Logincomponent = require("./usuario/login/login.component");
+import LoginComponent = Logincomponent.LoginComponent;
 
 @NgModule({
   declarations: [
@@ -16,7 +19,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    ProdutoComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,6 +30,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'produto', component: ProdutoComponent },
+      { path: 'entar', component: LoginComponent}
     ])
   ],
   providers: [],
